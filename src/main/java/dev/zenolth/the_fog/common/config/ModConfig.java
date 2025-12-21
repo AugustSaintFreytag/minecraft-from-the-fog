29aae0375d92fd2f0ff5ad63af04244145405780
August Saint Freytag <me@augustfreytag.com>
2025-12-21 12:36:43 +0100

    Add config option to set needed sightings

--- src/main/java/dev/zenolth/the_fog/common/config/ModConfig.java
+++ src/main/java/dev/zenolth/the_fog/common/config/ModConfig.java
@@ -34,6 +34,7 @@ public class ModConfig extends Config {
     public static class BehaviorSection extends ConfigSection {
         @ValidatedFloat.Restrict(min = 1f,max = 10f) public float speedMultiplier = 1f;
         @ValidatedInt.Restrict(min = 1,max = 120) public int forgetTime = 5;
+        @ValidatedInt.Restrict(min = 0,max = 20) public int sightingsBeforeChase = 1;
         @ValidatedInt.Restrict(min = 1,max = 360) public int minAliveTime = 10;
         @ValidatedInt.Restrict(min = 1,max = 360) public int maxAliveTime = 30;
     }
